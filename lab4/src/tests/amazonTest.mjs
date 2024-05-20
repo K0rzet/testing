5b6792ff30e1ce8b32719f0a29b111c77d7117c3
K0rzet <ehot2004@gmail.com>
2024-05-20 15:26:36 +0300

    lab4

---
+++ lab4/src/tests/amazonTest.mjs
@@ -0,0 +1,68 @@
+import { Browser, Builder } from "selenium-webdriver";
+import { expect } from "chai";
+import HomePage from "../pages/homePage.mjs";
+import ProductPage from "../pages/ProductPage.mjs";
+import SearchResultsPage from "../pages/SearchResultsPage.mjs";
+import { takeScreenshot } from "../utils/screenshot.mjs";
+
+describe("Amazon Smoke Tests", function () {
+  this.timeout(30000);
+  let driver;
+  let homePage;
+  let searchResultsPage;
+  let productPage;
+
+  before(async () => {
+    driver = new Builder().forBrowser(Browser.EDGE).build();
+    await driver.manage().window().maximize();
+    homePage = new HomePage(driver);
+    searchResultsPage = new SearchResultsPage(driver);
+    productPage = new ProductPage(driver);
+  });
+
+  after(async () => {
+    await driver.quit();
+  });
+
+  it("should open the Amazon home page", async () => {
+    try {
+      await homePage.open();
+      await driver.sleep(10000);
+      const title = await homePage.getTitle();
+      expect(title).to.include("Amazon");
+    } catch (error) {
+      await takeScreenshot(driver, testName);
+      throw error;
+    }
+  });
+
+  it("should search for a product", async () => {
+    try {
+      await homePage.open();
+      await homePage.searchForItem("laptop");
+      await searchResultsPage.waitForResults();
+      const firstProductText = await searchResultsPage.getFirstProductText();
+      console.log(firstProductText);
+      expect(firstProductText.toLowerCase()).to.include("laptop");
+    } catch (error) {
+      await takeScreenshot(driver, testName);
+      throw error;
+    }
+  });
+
+  it("should add a product to the cart", async () => {
+    try {
+      await homePage.open();
+      await homePage.searchForItem("laptop");
+      await searchResultsPage.waitForResults();
+      await searchResultsPage.selectFirstProduct();
+      await productPage.addToCart();
+      await productPage.verifyProductAdded();
+      const confirmationText = await productPage.getConfirmationText();
+      expect(confirmationText).to.include("Added to Cart");
+    } catch (error) {
+      await takeScreenshot(driver, testName);
+      throw error;
+    }
+  });
+});
